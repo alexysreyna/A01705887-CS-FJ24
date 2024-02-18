f1e73f44047ba5acfcba967c625468e374ace5ee
Alexys Armando Reyna Rodríguez <alexys_reyna@MacBook-Air-de-Alexys.local>
2024-02-18 16:53:51 -0600

    Lab 1 y 3 completados, trabajo en css y formato de HTML.

---
+++ codigo.js
@@ -0,0 +1,27 @@
+//console (log, info, warn, error, assert)
+
+console.log("hola");
+console.info("Esto es información");
+console.warn("Esto es una advertencia");
+console.error("Esto es un error");
+
+//Sirve para hacer pruebas.
+console.assert(1==1);
+
+//Forma antigua de declarar variables
+var juego = "Minecraft"
+
+//Constantes
+const precio = 100;
+
+//Forma moderna y más segura de declarar variables
+let cantidad = 1;
+
+alert("Confirmar eliminar datos");
+
+const nombre = prompt("¿Cómo te llamas?");
+
+console.log("Hola" + nombre);
+
+
+
